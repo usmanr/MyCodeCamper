68a5f00f0079ec8090b5297366d300cf19f436be
usmanr <usmanr@gmail.com>
2015-10-23 07:15:19 +1100

    initial commit

---
+++ CodeCamper/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CodeCamper.Startup))]
+namespace CodeCamper
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
